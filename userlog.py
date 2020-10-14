035e231706c09f8e6ca80744fa39d2c417269b4d
nabih.benazzouz <raefkoo@gmail.com>
2020-10-14 16:56:42 +0200

    {clean} clean code and add a file containing userlog class

---
+++ userlog.py
@@ -0,0 +1,8 @@
+class userlog:
+    def __init__(self, dc_name, computer_name, account_name, account_password,
+                                                                        dc_ip):
+        self.dc_name = dc_name
+        self.computer_name = computer_name
+        self.account_name = account_name
+        self.account_password = account_password
+        self.dc_ip = dc_ip
